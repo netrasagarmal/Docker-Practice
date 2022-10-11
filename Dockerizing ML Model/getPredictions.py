c3a1ab50629102fce1d4cf6e30c76e9374611504
Sagar Patil <55647831+netrasagarmal@users.noreply.github.com>
2022-10-11 11:33:49 +0530

    Running ml model

--- Dockerizing ML Model/getPredictions.py
+++ Dockerizing ML Model/getPredictions.py
@@ -5,11 +5,19 @@ import numpy as np
 with open('SVM.pickle', 'rb') as f:
     model = pickle.load(f)
 
-sepal_length = float(input("sepal_length: \t"))
-sepal_width = float(input("sepal_width: \t"))
-petal_length = float(input("petal_length: \t"))
-petal_width = float(input("petal_width: \t"))
 
-X_new = np.array([[sepal_length,sepal_width,petal_length,petal_width],[  4.9, 2.2, 3.8, 1.1 ], [  5.3, 2.5, 4.6, 1.9 ]])
 
-print(model.predict(X_new))
+try:
+    # Load the model
+    with open('SVM.pickle', 'rb') as f:
+        model = pickle.load(f)
+    sepal_length = float(input("sepal_length: \t"))
+    sepal_width = float(input("sepal_width: \t"))
+    petal_length = float(input("petal_length: \t"))
+    petal_width = float(input("petal_width: \t"))
+
+    X_new = np.array([[sepal_length,sepal_width,petal_length,petal_width],[  4.9, 2.2, 3.8, 1.1 ], [  5.3, 2.5, 4.6, 1.9 ]])
+
+    print(model.predict(X_new))
+except:
+    print("EOF")
---
+++ Python Docker/run.py
@@ -0,0 +1,7 @@
+
+try:
+    name = input("Please enter your name: \t")
+
+    print("Entered Name is:\t"+name)
+except:
+    print("EOF")
